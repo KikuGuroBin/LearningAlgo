518e7994b104ba8ae7c80789427aa328556bd5f0
龍生 小林 <kss-15180151@c-league.jp>
2017-11-09 20:17:38 +0900

    2017/11/09　形状□タイプの基礎計算アルゴリズム完成

--- LearningAlgo/LearningAlgo/Test1.xaml.cs
+++ LearningAlgo/LearningAlgo/Test1.xaml.cs
@@ -10,24 +10,31 @@ using Xamarin.Forms.Xaml;
 
 namespace LearningAlgo
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Test1 : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Test1 : ContentPage
+    {
 
         // DBにする配列達
         string[] FlowTable = new string[3];
         string[] PrintTable = new string[4];
         string[] KindTable = new string[3];
 
+        // 今後i,jとか数値が変化するやつをハッシュマップで管理したい
+        Dictionary<string, int> VarManagement = new Dictionary<string, int>();
 
-        public Test1 ()
-		{
-			InitializeComponent ();
+        ArrayDeque NumberStack = new ArrayDeque();
+        ArrayDeque SymbolStack = new ArrayDeque();
+
+
+
+        public Test1()
+        {
+            InitializeComponent();
 
             // プログラムでのレイアウト追加方法
             Label TestNo1Label = new Label
             {
-                Text = "1→i"
+                Text = "3＋12×4－6÷2→i"
             };
             Label TestNo2Label = new Label
             {
@@ -46,84 +53,130 @@ namespace LearningAlgo
 
 
             // 判定後”□”の場合
-            char[] StackText = TestNo1Label.Text.ToCharArray();
-            bool Calculat = false;
-            bool Tenmade = false;
-            ArrayDeque NumberStack = new ArrayDeque();
-            ArrayDeque SymbolStack = new ArrayDeque();
+            char[] StackText = TestNo1Label.Text.ToString().ToCharArray();
 
-            for (int length = 0 ; length < StackText.Length ; length--)
+            // 最終的な変数　→i　の i的な
+            string FinalizdKey;
+            FinalizdKey = (StackText[(StackText.Length - 1)]).ToString();
+
+            System.Diagnostics.Debug.WriteLine(TestNo1Label.Text.ToString());
+
+
+
+            System.Diagnostics.Debug.WriteLine(StackText.Length);
+            //与えられた文字列を一文字ずつ分解したから初めから数字と記号でスタックしてく(×÷は計算する)　→　が来たらブレイク
+            for (int Tenmade = 0, length = 0, Calculat = 0, Popcount = 0; length < StackText.Length; length++)
             {
-                if (Calculat==true)
+
+                System.Diagnostics.Debug.WriteLine("何週目ですか:" + length);
+
+                if (StackText[length].ToString().Equals("→"))
                 {
-
-                    Calculat = false;
-                    string Symbol = SymbolStack.Pop().ToString();
-                    if (Symbol.Equals("×"))
+                    for (Popcount++; Popcount >= 0; Popcount--)
                     {
-                        NumberStack.Push(int.Parse(NumberStack.Pop().ToString()) * int.Parse(StackText[length].ToString()));
+                        CalculationMethod();
                     }
-                    else if (Symbol.Equals("÷"))
-                    {
-                        NumberStack.Push(int.Parse(NumberStack.Pop().ToString()) / int.Parse(StackText[length].ToString()));
-
-                    }
-
-
-                    int a = int.Parse(NumberStack.Pop().ToString());
-
-                }
-                else if (StackText[length].Equals("→"))
-                {
-
                     break;
                 }
-
                 else
                 {
-                    if (StackText[length].Equals("×") || StackText[length].Equals("÷") ||
-                        StackText[length].Equals("＋") || StackText[length].Equals("－"))
+                    if (StackText[length].ToString().Equals("×") || StackText[length].ToString().Equals("÷"))
                     {
-                        SymbolStack.Push(StackText[length]);
-                        Tenmade = false;
-                        Calculat = true;
-
-                    }
-                    else
-                    {
-                        if (Tenmade == true)
+                        if (Calculat == 1)
                         {
-                            NumberStack.Push(int.Parse(NumberStack.Pop().ToString()) * 10 + StackText[length]);
+                            Popcount++;
+                        }
+
+                        if (Calculat >= 2)
+                        {
+                            CalculationMethod();
+
+                        }
+
+                        SymbolStack.Push(StackText[length]);
+                        System.Diagnostics.Debug.WriteLine("中身：" + SymbolStack.ToString());
+                        System.Diagnostics.Debug.WriteLine("数値中身：" + NumberStack.ToString());
+                        Calculat = 2;
+                        Tenmade = 0;
+                    }
+                    else if (StackText[length].ToString().Equals("＋") || StackText[length].ToString().Equals("－"))
+                    {
+                        if (Calculat >= 1)
+                        {
+
+                            for (; Popcount >= 0; Popcount--)
+                            {
+                                CalculationMethod();
+                            }
+
+                        }
+                        Calculat = 1;
+                        SymbolStack.Push(StackText[length]);
+                        System.Diagnostics.Debug.WriteLine("中身：" + SymbolStack.ToString());
+                        System.Diagnostics.Debug.WriteLine("数値中身：" + NumberStack.ToString());
+                        Tenmade = 0;
+                    }
+                    else if (StackText[length].ToString().Equals("i") || StackText[length].ToString().Equals("j"))
+                    {
+                        if (!VarManagement.ContainsKey(StackText[length].ToString()))
+                        {
+                            VarManagement.Add(StackText[length].ToString(), 0);
                         }
                         else
                         {
                             NumberStack.Push(StackText[length]);
                         }
-                        Tenmade = true;
-
                     }
+                    else
+                    {
+                        if (Tenmade == 1)
+                        {
 
+                            System.Diagnostics.Debug.WriteLine("a");
 
+                            NumberStack.Push(int.Parse(NumberStack.Pop().ToString()) * 10 + int.Parse(StackText[length].ToString()));
+                        }
+                        else
+                        {
+                            NumberStack.Push(StackText[length]);
+                        }
+                        Tenmade = 1;
 
-
-
+                        System.Diagnostics.Debug.WriteLine("中身：" + SymbolStack.ToString());
+                        System.Diagnostics.Debug.WriteLine("数値中身：" + NumberStack.ToString());
+                    }
                 }
-                
-
-
-
-
-                if(StackText[length].Equals("×") || StackText[length].Equals("÷")){
-                    Calculat = true;
-
-                }
-                
-
-
+            }
+            System.Diagnostics.Debug.WriteLine("最終結果：" + NumberStack.Peek());
+        }
+        public void CalculationMethod()
+        {
+            if (SymbolStack.Peek().ToString().Equals("×"))
+            {
+                SymbolStack.Pop();
+                string Before = NumberStack.Pop().ToString();
+                NumberStack.Push(int.Parse(NumberStack.Pop().ToString()) * int.Parse(Before));
+            }
+            else if (SymbolStack.Peek().ToString().Equals("÷"))
+            {
+                SymbolStack.Pop();
+                string Before = NumberStack.Pop().ToString();
+                NumberStack.Push(int.Parse(NumberStack.Pop().ToString()) / int.Parse(Before));
 
             }
+            else if (SymbolStack.Peek().ToString().Equals("＋"))
+            {
+                SymbolStack.Pop();
+                string Before = NumberStack.Pop().ToString();
+                NumberStack.Push(int.Parse(NumberStack.Pop().ToString()) + int.Parse(Before));
+            }
+            else if (SymbolStack.Peek().ToString().Equals("－"))
+            {
+                SymbolStack.Pop();
+                string Before = NumberStack.Pop().ToString();
+                NumberStack.Push(int.Parse(NumberStack.Pop().ToString()) - int.Parse(Before));
 
-
+            }
         }
-	}
+    }
 }
